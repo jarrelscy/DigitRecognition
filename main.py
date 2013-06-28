cf013d5655e1f030a108ff739af1ccfb7b085b4a
jarrelscy <jarrelscy@gmail.com>
2013-06-29 00:38:28 +0800

    addedutils

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ Created on 27/06/2013
 
 @author: jarrelscy, justinpoh
 '''
+import numpy
+import scipy
+import time
+from utils import *
 
-if __name__ == '__main__':
-    pass
-    
-    test test
+
+
+## Please note that you have to download train.csv for this to work duh
+#setup file with training csv
+dataObject = database("train.csv", max=100)
+dataObject.images[2].displayImage()
+
+time.sleep(30) #hack for my poor windows which can't display images without waiting else the temp cache is cleared -.-'
---
+++ utils.py
@@ -0,0 +1,49 @@
+'''
+Created on 28/06/2013
+
+@author: jarrelscy
+'''
+import numpy as np
+import Image
+class image:
+    def __init__(self, data, answer):
+        self.image =  np.reshape(np.array(data), (28,28))
+        self.answer = answer
+    def getPixelAt(self, y, x): #note that its y then x, this is just the python way
+        # the arrays look like this: 
+        #  [ [0,0,1,0],
+        #    [0,1,0,0],
+        #    [1,0,0,0] ]
+        # 
+        return self.image[y][x]
+    def displayImage(self):
+        Image.fromarray(self.image).show()    
+class database:
+    '''
+    classdocs
+    '''
+    
+
+    def __init__(self, filename, max=42000):
+        '''
+        Constructor
+        '''
+        self.numberOfImages = 0
+        f = open(filename, 'r')        
+        self.images = [] #this stores all the images
+        f.readline() #get rid of first line
+        
+        for line in f:
+            l = map(int, line.split(","))
+            answer = l.pop(0) #remove the answer  from the start            
+            self.images.append(image(l, answer))
+            self.numberOfImages = self.numberOfImages + 1
+            if(self.numberOfImages % 100 == 0):
+                print self.numberOfImages, 'images have been loaded'
+            if(self.numberOfImages > max):
+                break
+    def displayAnswerOf(self, index):
+        print self.images[index].answer
+        
+        
+        
